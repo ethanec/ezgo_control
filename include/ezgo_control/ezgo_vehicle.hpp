c6f2d1e73801738ed0550c1498a2eb7f29bb19ff
ethanec <ethanec@gmail.com>
2022-08-02 17:55:11 +0800

    Add parameters and launch file
    1. Add parameters for PID controller
    2. Intergrate .cpp files to a launch file

--- include/ezgo_control/ezgo_vehicle.hpp
+++ include/ezgo_control/ezgo_vehicle.hpp
@@ -15,6 +15,11 @@
 #include <tablet_socket_msgs/gear_cmd.h>
 #include <tablet_socket_msgs/mode_cmd.h>
 
+#include <autoware_can_msgs/CANInfo.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <tablet_socket_msgs/mode_info.h>
+
 #define RESET "\033[0m"
 #define BLACK "\033[30m"   /* Black */
 #define RED "\033[31m"     /* Red */
@@ -26,10 +31,49 @@
 #define WHITE "\033[37m"   /* White */
 
 enum Light { OFF, ON };
-
 enum Shift { FORWARD, REVERSE };
-
 enum Mode { MANUAL, AUTONOMOUS };
+
+typedef struct vehicle_config {
+    bool is_valid;
+    int vendor_ID;
+    int car_ID;
+    int CAN_dev_ID;
+    double SPEED_LIMIT;
+
+    // steering
+    double STEERING_ANGLE_MAX;
+    double TURNING_RADIUS_MIN;
+    double WHEEL_BASE;
+    double WHEEL_ANGLE_MAX;
+
+    // accel
+    double _K_ACCEL_P_UNTIL30;
+    double _K_ACCEL_I_UNTIL30;
+    double _K_ACCEL_D_UNTIL30;
+    double _K_ACCEL_P_UNTIL15;
+    double _K_ACCEL_I_UNTIL15;
+    double _K_ACCEL_D_UNTIL15;
+
+    double _K_ACCEL_I_GAIN;
+    double _K_ACCEL_OFFSET;
+    double _ACCEL_MAX_I;
+    double _ACCEL_PEDAL_MAX;
+    double _ACCEL_PEDAL_MIN;
+
+    // brake
+    double _K_BRAKE_P;
+    double _K_BRAKE_I;
+    double _K_BRAKE_D;
+    int _K_BRAKE_I_CYCLES;
+    double _BRAKE_MAX_I;
+    double _BRAKE_PEDAL_MAX;
+    double _BRAKE_PEDAL_MIN;
+    double _BRAKE_PEDAL_STOPPING_MAX;
+    double _BRAKE_PEDAL_STOPPING_MED;
+    double _BRAKE_PEDAL_OFFSET;
+
+} vehicle_config_t;
 
 typedef struct vehicle_info {
     int throttle;
@@ -54,9 +98,14 @@ typedef struct vehicle_cmd {
 
 extern vehicle_cmd_t vehicle_cmd;
 extern vehicle_info_t vehicle_info;
+extern vehicle_config_t vehicle_config;
 extern int willExit;
 
-void cmd_reset()
+void set_drv_stroke(double accel_stroke);
+void set_break_stroke(double brake_stroke);
+void PedalControl(double current_vel, double cmd_vel);
+
+inline void cmd_reset()
 {
     vehicle_cmd.linear_x = 0;
     vehicle_cmd.angular_z = 0;
@@ -68,7 +117,7 @@ void cmd_reset()
     vehicle_cmd.light = 0;
 }
 
-void modeCMDCallback(const tablet_socket_msgs::mode_cmd &mode)
+inline void modeCMDCallback(const tablet_socket_msgs::mode_cmd &mode)
 {   
     if (mode.mode == -1 || mode.mode == 0) {
         cmd_reset();
@@ -77,39 +126,39 @@ void modeCMDCallback(const tablet_socket_msgs::mode_cmd &mode)
     std::cout << "In modeCMDCallback, mode = " << vehicle_cmd.modeValue << std::endl;
 }
 
-void gearCMDCallback(const tablet_socket_msgs::gear_cmd &gear)
+inline void gearCMDCallback(const tablet_socket_msgs::gear_cmd &gear)
 {
     vehicle_cmd.shift = gear.gear;
     std::cout << "In gearCMDCallback, mode = " <<  vehicle_cmd.shift << std::endl;
 }
 
-void twistCMDCallback(const geometry_msgs::TwistStamped &msg)
+inline void twistCMDCallback(const geometry_msgs::TwistStamped &msg)
 {
     vehicle_cmd.linear_x = msg.twist.linear.x;
     vehicle_cmd.angular_z = msg.twist.angular.z;
 }
 
-void steerCMDCallback(const autoware_msgs::SteerCmd &steer)
+inline void steerCMDCallback(const autoware_msgs::SteerCmd &steer)
 {
     vehicle_cmd.steering_angle = steer.steer;
 }
 
-void accellCMDCallback(const autoware_msgs::AccelCmd &accell)
+inline void accellCMDCallback(const autoware_msgs::AccelCmd &accell)
 {
     vehicle_cmd.accel_stroke = accell.accel;
 }
 
-void brakeCMDCallback(const autoware_msgs::BrakeCmd &brake)
+inline void brakeCMDCallback(const autoware_msgs::BrakeCmd &brake)
 {
     vehicle_cmd.brake_stroke = brake.brake;
 }
 
-void vehicle_control()
+inline void vehicle_control()
 {
     // TODO
 }
 
-void showVehicleInfo()
+inline void showVehicleInfo()
 {
     if (vehicle_info.control_mode == MANUAL) {
         std::cout << GREEN << "------ manual mode ------" << RESET << std::endl;
@@ -134,15 +183,15 @@ void showVehicleInfo()
     return;
 }
 
-char app_sigaltstack[SIGSTKSZ];
+inline char app_sigaltstack[SIGSTKSZ];
 
-void app_signal_handler(int sig_num)
+inline void app_signal_handler(int sig_num)
 {
     willExit = 1;
     ros::shutdown();
 }
 
-int app_setup_signals()
+inline int app_setup_signals()
 {
     stack_t sigstack;
     struct sigaction sa;
--- src/arduino_control.cpp
+++ src/arduino_control.cpp
@@ -4,10 +4,100 @@
 using namespace mn::CppLinuxSerial;
 
 pthread_mutex_t mutex;
+vehicle_config_t vehicle_config;
 vehicle_info_t vehicle_info;
 vehicle_cmd_t vehicle_cmd;
 int willExit = 0;
 
+ros::Publisher serial_pub;
+ros::Publisher mode_pub;
+
+// writer
+// void SteeringControl(float cmd_steering_angle)
+// {
+//     // TODO TODO TODO FIXME: positive angle == left
+//     int16_t int16_angle = (int16_t) cmd_steering_angle;
+//     static char msg[2];
+
+//     // Byte order: Big-endian
+//     // high byte
+//     msg[0] = (int16_angle >> 8) & 0x00ff;
+//     // low byte
+//     msg[1] = int16_angle & 0x00ff;
+// }
+
+static std::vector<uint8_t> setVehicleGearDrv()
+{   
+    std::vector<uint8_t> msg;
+    
+    // gear
+    char current_gear = vehicle_info.shift;
+    char cmd_gear = vehicle_cmd.shift;
+    float speed = vehicle_info.velocity;
+
+    if(cmd_gear == current_gear){
+        msg[2] = (uint8_t) current_gear;
+    }else if(cmd_gear != current_gear && speed == 0.){
+        msg[2] = (uint8_t) cmd_gear;
+    }
+
+    // drv
+    // #define WHEEL_TO_STEERING (v_config.STEERING_ANGLE_MAX / v_config.WHEEL_ANGLE_MAX)
+    // static float pre_cmd_steering_angle = 0.0;
+    // float cmd_steering_angle;
+    float cmd_velocity = vehicle_cmd.linear_x;
+    float current_velocity = vehicle_info.velocity;
+
+    // if (fabs(v_cmd.linear_x) < 0.1) {  // just avoid divided by zero.
+    //     cmd_steering_angle = pre_cmd_steering_angle;
+    // } else {
+    //     double phi_angle_pi = (v_cmd.angular_z / v_cmd.linear_x);
+    //     double wheel_angle_pi = phi_angle_pi * v_config.WHEEL_BASE;
+    //     double wheel_angle = (wheel_angle_pi / M_PI) * 180.0;
+    //     cmd_steering_angle = wheel_angle * WHEEL_TO_STEERING;
+
+
+    //     // Limit the steering angle
+    //     if (cmd_steering_angle < -v_config.STEERING_ANGLE_MAX)
+    //         cmd_steering_angle = -v_config.STEERING_ANGLE_MAX;
+    //     if (cmd_steering_angle > v_config.STEERING_ANGLE_MAX)
+    //         cmd_steering_angle = v_config.STEERING_ANGLE_MAX;
+
+    //     pre_cmd_steering_angle = cmd_steering_angle;
+    // }
+
+    PedalControl((double) current_velocity, (double) cmd_velocity);
+    // SteeringControl(cmd_steering_angle);
+
+    msg[0] = (uint8_t) vehicle_cmd.accel_stroke;
+    msg[1] = (uint8_t) vehicle_cmd.brake_stroke;
+
+    return msg;
+}
+
+// reader
+static void publish_serial_msg()
+{
+    autoware_can_msgs::CANInfo serial_msg;
+
+    serial_msg.header.frame_id = "/serail_port";
+    serial_msg.header.stamp = ros::Time::now();
+    serial_msg.drivepedal = vehicle_info.throttle;
+    serial_msg.brakepedal = vehicle_info.brake;
+    // serial_msg.angle = 
+    serial_msg.speed = vehicle_info.velocity;
+    serial_msg.driveshift = vehicle_info.shift;
+    serial_msg.light = vehicle_info.light;
+    serial_pub.publish(serial_msg);
+
+    tablet_socket_msgs::mode_info mode_msg;
+    mode_msg.header.frame_id = "/mode";
+    mode_msg.header.stamp = ros::Time::now();
+    mode_msg.mode = vehicle_info.control_mode;
+    mode_pub.publish(mode_msg);
+}
+
+// main
 static void *writer_handler(void *args)
 {
     std::cout << YELLOW << "ENTER ezgo_vehicle_control Writer thread.\n" << RESET << std::endl;
@@ -16,28 +106,26 @@ static void *writer_handler(void *args)
 	
     while (ros::ok() && !willExit) {
         ros::spinOnce();
+        std::vector<uint8_t> data;
+        std::vector<uint8_t> pub_data;
         switch (vehicle_cmd.modeValue) {
-        case 0:
-            // std::cout << "In case 0.\n" << std::endl;
+        case 0: // manual mode
             cmd_reset();
             break;
         case 1:  // autonomous mode
-            // std::cout << "In case 1.\n" << std::endl;
-            cmd_reset();
+            pub_data = setVehicleGearDrv();
+            pthread_mutex_lock(&mutex);
+            serialPort->WriteBinary(pub_data);
+            pthread_mutex_unlock(&mutex);
             break;
         case 2:  // UI direct control
-            // std::cout << "In case 2.\n" << std::endl;
-            std::vector<uint8_t> data;
             data.push_back((uint8_t) vehicle_cmd.accel_stroke);
             data.push_back((uint8_t) vehicle_cmd.brake_stroke);
             data.push_back((uint8_t) vehicle_cmd.shift);
             pthread_mutex_lock(&mutex);
             serialPort->WriteBinary(data);
-            // for (int i = 0; i < data.size(); i++){
-            //     std::cout << "data[" << i << "]: " << unsigned(data[i]) << ", ";
-            // }
-            std::cout<< std::endl;
             pthread_mutex_unlock(&mutex);
+            data.clear();
             break;
         }
     }
@@ -66,6 +154,7 @@ static void *reader_handler(void *args)
             vel |= ((uint16_t) readData[4]) & 0xFF;
             vehicle_info.velocity = ((float) vel) / 1000;
             showVehicleInfo();
+            publish_serial_msg();
         } else {
             std::cout << RED << "Without Receive DATA, Check connect ...." << RESET << std::endl;
         }
@@ -82,7 +171,7 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    ros::init(argc, argv, "arduino_control");
+    ros::init(argc, argv, "arduino_ezgo");
     ros::NodeHandle nh;
 
     ros::Subscriber sub[6];
@@ -92,6 +181,9 @@ int main(int argc, char **argv)
     sub[3] = nh.subscribe("/accel_cmd", 1, accellCMDCallback);
     sub[4] = nh.subscribe("/steer_cmd", 1, steerCMDCallback);
     sub[5] = nh.subscribe("/brake_cmd", 1, brakeCMDCallback);
+
+    serial_pub = nh.advertise<autoware_can_msgs::CANInfo>("serial_info", 100);
+    mode_pub = nh.advertise<tablet_socket_msgs::mode_info>("mode_info", 100);
 
     pthread_t thread_writer;
     pthread_t thread_reader;
